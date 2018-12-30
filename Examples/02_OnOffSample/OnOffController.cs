a40cfb7e02f88f541ed7823424dea6d806c9c636
Kirurobo <kirurobo@gmail.com>
2018-12-30 16:18:55 +0900

    エディタの場合、Gameビューを自ウィンドウとして探すようにした。 エディタ上dえDragMoveが働かなかったのを修正。

--- Examples/02_OnOffSample/OnOffController.cs
+++ Examples/02_OnOffSample/OnOffController.cs
@@ -5,46 +5,67 @@ using UnityEngine.UI;
 
 namespace Kirurobo
 {
-	/// <summary>
-	/// WindowControllerの設定を単純にオン／オフするサンプル
-	/// </summary>
-	public class OnOffController : MonoBehaviour
-	{
-		WindowController windowController;
+    /// <summary>
+    /// WindowControllerの設定を単純にオン／オフするサンプル
+    /// </summary>
+    public class OnOffController : MonoBehaviour
+    {
+        WindowController windowController;
 
-		public Toggle transparentToggle;
-		public Toggle topmostToggle;
-		public Toggle maximizedToggle;
-		public Toggle minimizedToggle;
-		public Toggle enableFileDropToggle;
-		public Text droppedFilesText;
+        public Toggle transparentToggle;
+        public Toggle topmostToggle;
+        public Toggle maximizedToggle;
+        public Toggle minimizedToggle;
+        public Toggle enableFileDropToggle;
+        public Text droppedFilesText;
 
-		// Use this for initialization
-		void Start()
-		{
-			// 同じゲームオブジェクトに WindowController をアタッチしておくこと
-			windowController = GetComponent<WindowController>();
+        // Use this for initialization
+        void Start()
+        {
+            // 同じゲームオブジェクトに WindowController をアタッチしておくこと
+            windowController = GetComponent<WindowController>();
 
-			// ファイルドロップ時の処理
-			windowController.OnFilesDropped += (string[] files) =>
-			{
-				if (droppedFilesText)
-				{
-				// ドロップされたファイルのパスを表示
-				droppedFilesText.text = string.Join("\n", files);
-				}
-			};
+            // ファイルドロップ時の処理
+            windowController.OnFilesDropped += (string[] files) =>
+            {
+                if (droppedFilesText)
+                {
+                    // ドロップされたファイルのパスを表示
+                    droppedFilesText.text = string.Join("\n", files);
+                }
+            };
 
-			if (transparentToggle) transparentToggle.isOn = windowController.isTransparent;
-			if (topmostToggle) topmostToggle.isOn = windowController.isTopmost;
-			if (maximizedToggle) maximizedToggle.isOn = windowController.isMaximized;
-			if (minimizedToggle) minimizedToggle.isOn = windowController.isMinimized;
-			if (enableFileDropToggle) enableFileDropToggle.isOn = windowController.enableFileDrop;
-		}
+            if (transparentToggle)
+            {
+                transparentToggle.isOn = windowController.isTransparent;
+                transparentToggle.onValueChanged.AddListener(val => windowController.isTransparent = val);
+            }
+            if (topmostToggle)
+            {
+                topmostToggle.isOn = windowController.isTopmost;
+                topmostToggle.onValueChanged.AddListener(val => windowController.isTopmost = val);
+            }
+            if (maximizedToggle)
+            {
+                maximizedToggle.isOn = windowController.isMaximized;
+                maximizedToggle.onValueChanged.AddListener(val => windowController.isMaximized = val);
+            }
+            if (minimizedToggle)
+            {
+                minimizedToggle.isOn = windowController.isMinimized;
+                minimizedToggle.onValueChanged.AddListener(val => windowController.isMinimized = val);
+            }
 
-		// Update is called once per frame
-		void Update()
-		{
-		}
-	}
+            if (enableFileDropToggle)
+            {
+                enableFileDropToggle.isOn = windowController.enableFileDrop;
+                enableFileDropToggle.onValueChanged.AddListener(val => windowController.enableFileDrop = val);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
 }
